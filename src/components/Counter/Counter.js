5f3cc7225a1607d6822e4bf897280d4995533f53
Jinjerana <125341676+Jinjerana@users.noreply.github.com>
2023-09-01 22:42:54 +0200

    HW2_1_3

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,23 +1,18 @@
 import React from "react"
 
-import Controls from "./Controls";
-
-
 const Counter = ({ good, neutral, bad }) => {
     
     return (
-        <div>
-            
-            <div className="Controls">
-         <button type="button" onClick={good}>
-        Goog</button>
-        <button type="button" onClick={neutral}>
-        Neutral</button>
-        <button type="button" onClick={bad}>
-        Bad</button>
+        <div> 
+            <div className="Counter">
+                <button type="button" onClick={good}>
+                    Goog</button>
+                <button type="button" onClick={neutral}>
+                    Neutral</button>
+                <button type="button" onClick={bad}>
+                    Bad</button>
             </div>
-      
-    </div>
+        </div>
   );
 };
 
